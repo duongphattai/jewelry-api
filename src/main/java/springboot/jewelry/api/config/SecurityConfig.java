fc57260626602ff18c0dd1db6ebbd118aa8ed3ee
Phat Tai <dptai7@gmail.com>
2021-08-03 15:05:42 +0700

    Update SecurityConfig.java

--- src/main/java/springboot/jewelry/api/config/SecurityConfig.java
+++ src/main/java/springboot/jewelry/api/config/SecurityConfig.java
@@ -59,15 +59,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8088"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
     @Bean
     @Override
